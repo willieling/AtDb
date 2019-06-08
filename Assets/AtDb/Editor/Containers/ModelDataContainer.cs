97402f61bbdf2c78ad4c4dfc7ae6c6a387a92279
William Ling <monkeyhood@gmail.com>
2019-06-08 01:19:41 -0400

    remove comments

--- Assets/AtDb/Editor/Containers/ModelDataContainer.cs
+++ Assets/AtDb/Editor/Containers/ModelDataContainer.cs
@@ -9,35 +9,6 @@
         {
             this.data = data;
             this.metadata = metadata;
-
-
-            //FillModelWithData();
         }
-
-        //private void FillModelWithData()
-        //{
-        //    //todo move this functionality outisde the class
-        //    //create a factory to make ModelDataContainer
-
-        //    //todo pool modelFillers?
-        //    switch (tableData.metadata.Style)
-        //    {
-        //        case DataStyle.Direct:
-        //            modelFiller = new DirectModelFiller(classMaker, model, tableData);
-        //            break;
-        //        case DataStyle.List:
-        //            modelFiller = new ListModelFiller(classMaker, model, tableData);
-        //            break;
-        //        case DataStyle.Dictionary:
-        //            modelFiller = new DictionaryModelFiller(classMaker, model, tableData);
-        //            break;
-        //        default:
-        //            //todo better error checking
-        //            throw new Exception();
-        //    }
-
-        //    //todo move model and table data arguments to fill function
-        //    modelFiller.Fill();
-        //}
     }
 }
